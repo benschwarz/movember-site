03a59ccb1c18d55076c822b00b3fae073fd20fb7
Ben Schwarz <ben.schwarz@gmail.com>
2009-11-08 14:47:06 +0800

    Moved template into its own file and setup the app deps

--- app.rb
+++ app.rb
@@ -1,31 +1,14 @@
 require 'sinatra'
 require 'haml'
+require 'rdiscount'
 
-set :public, File.join(File.dirname(__FILE__), 'public')
+APP_ROOT = File.join(File.dirname(__FILE__))
+require 'mo'
+
+set :public, "#{APP_ROOT}/public"
 set :haml, {:format => :html5, :attr_wrapper => '"'}
 
 get '/' do
+  @mos = Mo.all
   haml :index
 end
-
-
-__END__
-@@index
-!!!
-%html{:lang => "en-au"}
-  %head
-    %title Ben Schwarz's movember site
-    %meta{:name => "author", :content => "Ben Schwarz"}
-    /[if IE]
-      %script{:src => " http://html5shiv.googlecode.com/svn/trunk/html5.js"}
-    
-    %link{:rel => "stylesheet", :href => "/css/reset.css", :type => "text/css", :media => "screen"}
-    %link{:rel => "stylesheet", :href => "/css/base.css", :type => "text/css", :media => "screen"}
-  %body
-    %section#wrapper
-      %header
-        %h1 
-          Ben Schwarz's
-          %em
-            Mo'vember charity site
-      
